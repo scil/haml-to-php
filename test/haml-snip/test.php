fe8ece55324ce6cc2f32d78f485d48a1549c11ef
scil <caoxie.zc@gmail.com>
2013-01-28 00:19:35 +0800

    snip test

---
+++ test/haml-snip/test.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+@ini_set('xdebug.max_nesting_level','600');
+
+define('SNIP_FILES',__DIR__ . '/test.snip.php');
+define('HAML_SNIP_CACHE',__DIR__.'/snip-cache');
+@mkdir(HAML_SNIP_CACHE, 0777, true);
+
+
+define('HAML_TEST_DIR',dirname(__FILE__).'/../haml');
+$jsondata=array(
+  dirname(__FILE__).'/tests.json',
+//  HAML_TEST_DIR.'/ruby-haml-3.0.24-tests.json',
+//  HAML_TEST_DIR.'/extra-tests.json',
+);
+
+require_once HAML_TEST_DIR.'/test.php';
---
+++ test/haml-snip/test.snip.php
@@ -0,0 +1,75 @@
+﻿<?php
+if(version_compare(PHP_VERSION, '5.3', '<'))
+{
+    return 'callable snips are defined by closure, requireing php 5.3 ;  you can use Variable functions instead';
+}
+
+$hello=<<<SNIP
+#head
+  .c1
+    hello
+SNIP;
+
+$hello1=<<<SNIP
+#head
+  @@@
+  .c1
+    hello
+SNIP;
+
+$hello2=<<<SNIP
+#head
+  @@@
+  .c1
+    hello
+    @@@
+SNIP;
+  
+$name=<<<SNIP
+#head
+  @@@pl1
+  .c1
+    hello
+    @@@pl2
+SNIP;
+  
+$mix=<<<SNIP
+#head
+  @@@
+  .c1
+    hello
+    @@@pl2
+  @@@
+SNIP;
+  
+$snip= <<<SNIP
+%div#head
+  hello world!
+  @@@
+  @@@name2
+  @@@name1
+  @@@
+SNIP;
+
+$hellocall=function($arg){
+    $name=isset($arg['name'])?$arg['name']:'wl';
+    $age=isset($arg['age'])?$arg['age']:'18';
+    return <<<SNIP
+.person#$name
+  .name $name
+  .age $age
+SNIP;
+};
+
+$hellocall1=function($arg){
+    $name=isset($arg['name'])?$arg['name']:'wl';
+    $age=isset($arg['age'])?$arg['age']:'18';
+    return <<<SNIP
+.person#$name
+  .name $name
+  .age $age
+@@@
+SNIP;
+};
+
+?>
